7348421f62f2946263287a9665abdde779fb4edd
Adnnann <adnanovcina1987@gmail.com>
2021-12-20 20:12:48 +0100

    Revised highscore functionality

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -7,9 +7,9 @@ const restart = () => {
     window.location.reload()
 }
    return createPortal(
-    <div id="gameOver" style={{width:'100%',height:"50%", backgroundColor:'black', 
-    visibility:"hidden", marginTop:"7%", textAlign:"center",position:"absolute",opacity:"0.8"}}>  
-        <Button onClick={restart} style={{width:"20%",height:"50px", marginTop:"10%"}}>Start New Game</Button>
+    <div id="gameOver" style={{width:'100%',height:"40%", backgroundColor:'black', 
+    visibility:"hidden", marginTop:"7%", textAlign:"center",position:"absolute",opacity:"0.9"}}>  
+        <Button onClick={restart} style={{width:"20%",height:"50px", marginTop:"6%",opacity:"1"}}>Start New Game</Button>
     </div>,
     document.getElementById('modal')
    )
