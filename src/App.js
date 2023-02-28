a33398a1c387a1a69c7c07b701cf65a28cf91b6c
Adnan Ovcina <adnanovcina1987@gmail.com>
2023-02-28 21:10:27 +0100

    Revised

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import { Container } from 'semantic-ui-react';
 import Game from './components/Game';
 import './App.css';
-
-
 function App() {
 
   return (  
-    <Game />  
+   
+   <Game />  
   )
 }
 
